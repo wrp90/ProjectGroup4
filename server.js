204380ea9d3ec54e27f580f557b2bd4d38dd6e3e
Joe Malone <joe.d.malone@gmail.com>
2021-03-13 13:22:12 -0600

    correct path associations

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 
 app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 
-app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+app.get('reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
 
 app.get('/api/currentRes', (req, res) => res.json(currentRes));
 
