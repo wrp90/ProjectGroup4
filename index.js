e93d43d04fcf8c81370763209a92b6ef21bf1deb
Will Papp <wpapp90@gmail.com>
2021-03-13 10:53:01 -0600

    added basic constructor

---
+++ index.js
@@ -0,0 +1,8 @@
+class Reservation {
+    constructor(table, ID, email, phone) {
+        this.table = table; 
+        this.ID = ID;
+        this.email = email;
+        this.phone = phone;
+    }
+}
